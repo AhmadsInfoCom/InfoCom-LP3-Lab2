8c49e60bbaba8f522cf9e3e9db1192367855f6f5
Ahmad Ghaemi <ah2177gh-s@student.lu.se>
2022-02-09 10:19:25 +0100

    ändrade i database: en if-sats så att man kollar vilken drone som har postat, och sparar i redis utifrån det.

--- webserver/database.py
+++ webserver/database.py
@@ -32,11 +32,15 @@ def drone():
     #Kanske:
     #Gör en if-sats, t.ex. if(droneID = drone1) så ska i = 1, och formattera strängen därefter, så drone{i}: t.ex. redis.server.set('drone{i}_status', drone_status),  för: ID, IP, status, longitude, latitude
     
-    drone_list = [droneIP, drone_status, drone_longitude, drone_latitude]   #droneID isn't needed in the list, right? 
+    if(droneID == drone1):
+        drone1_list = [droneIP, drone_status, drone_longitude, drone_latitude]   #droneID isn't needed in the list, right? 
+    else:
+        drone2_list = [droneIP, drone_status, drone_longitude, drone_latitude]   #droneID isn't needed in the list, right? 
     
-    
-    json_drone_list = json.dumps(drone_list)          #konverterar listan till en sträng: https://pythonexamples.org/python-list-to-json/
-    redis_server.set(droneID, json_drone_list)        #lägger in strängen i redis_server
+    json_drone1_list = json.dumps(drone1_list)          #konverterar listan till en sträng: https://pythonexamples.org/python-list-to-json/
+    json_drone2_list = json.dumps(drone2_list) 
+    redis_server.set(droneID, json_drone1_list)        #lägger in strängen i redis_server
+    redis_server.set(droneID, json_drone2_list)        #lägger in strängen i redis_server
     #json.loads(redis_server.get(droneID))   
     
     
