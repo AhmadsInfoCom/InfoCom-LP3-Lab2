93d47841625bbc2dc0b9701183f25367be357ea3
Ahmad Ghaemi <ah2177gh-s@student.lu.se>
2022-02-16 11:08:59 +0100

    writelines i simulator måste typecast til lstring

--- pi/simulator.py
+++ pi/simulator.py
@@ -71,7 +71,7 @@ if __name__ == "__main__":
     print(current_coords, from_coords, to_coords)
     drone_long, drone_lat = run(args.id ,current_coords, from_coords, to_coords, SERVER_URL)
     dronedest = open("dronedestination.txt", "w+")    #.txt?
-    dronedest.writelines([drone_long, drone_lat])   #värdena sparas i två rader
+    dronedest.writelines([str(drone_long), str(drone_lat)])   #värdena sparas i två rader
     dronedest.close()
     # drone_long and drone_lat is the final location when drlivery is completed, find a way save the value, and use it for the initial coordinates of next delivery
     #=============================================================================
